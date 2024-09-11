4e36ee3ecedad42ebec93266e74294e1397043bd
Hayder <haidar.hassan.ch@gmail.com>
2024-09-12 01:51:30 +0300

    copy button added

--- lib/screens/final_screen.dart
+++ lib/screens/final_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_training/providers/getFinalOrder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FinalScreen extends ConsumerWidget {
@@ -16,6 +17,35 @@ class FinalScreen extends ConsumerWidget {
               text,
               textDirection: TextDirection.rtl,
             ),
+            ElevatedButton(
+                onPressed: () {
+                  Clipboard.setData(ClipboardData(text: text));
+
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('تم نسخ الطلب بنجاح'),
+                          content: Container(
+                            height: MediaQuery.of(context).size.height / 5,
+                            width: MediaQuery.of(context).size.width / 4,
+                            child: Column(
+                              children: [
+                                Text(
+                                    textDirection: TextDirection.rtl,
+                                    'الرجاء العودة للبوت و لصق النص الذي تم نسخه في البوت ليتم التواصل معك من قبل موظف التوصيل'),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('حسناً'))
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                },
+                child: Text('Press to copy')),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pushReplacementNamed(context, '/');
